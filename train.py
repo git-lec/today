e717dc39a107800d104dbb8be2b4fc0b70be492b
eddie <chosm1792@gmail.com>
2022-10-20 05:32:20 +0900

    충돌 실험

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import pandas as pdff
 aa = 'main br'
 a = [100,200,300,400]
 b = {1,2,3,4}
-c= 'br branch'
-d= 'br branch 2step br'
+c= 'br 3번째'
+d= 'br branch '
 ee = "local에서 만들고 잇었지 main에 작업중이었어"
 
 
